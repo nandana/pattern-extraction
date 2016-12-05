e3102733919d6ad09c335a5d6dfa03e219601c83
nandana <nandana.cse@gmail.com>
2016-12-05 20:05:50 +0100

    SSN analysis scripts.

--- src/main/java/es/upm/oeg/tools/rdfshapes/ssn/DatasetCheck.java
+++ src/main/java/es/upm/oeg/tools/rdfshapes/ssn/DatasetCheck.java
@@ -4,6 +4,7 @@ import com.hp.hpl.jena.query.ParameterizedSparqlString;
 import es.upm.oeg.tools.rdfshapes.utils.IOUtils;
 import es.upm.oeg.tools.rdfshapes.utils.SparqlUtils;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -29,42 +30,48 @@ import java.util.List;
  */
 public class DatasetCheck {
 
+
+    public static final String classQuery;
+
+    public static final String propertyQuery;
+
+    public static final String lodCache = "http://lod.openlinksw.com/sparql";
+
+    public static final String aemetSparql = "http://4v.dia.fi.upm.es:9191/sparql";
+
+    static {
+        try{
+            propertyQuery=IOUtils.readFile("ssn/prop.rq",Charset.defaultCharset());
+            classQuery = IOUtils.readFile("ssn/class.rq",Charset.defaultCharset());
+        } catch (IOException e){
+            throw new IllegalStateException("Error loading the queries ...");
+        }
+    }
+
+
     public static void main(String[] args) throws Exception {
 
-        String lodCache = "http://lod.openlinksw.com/sparql";
 
-        List<String> classList = Files.
-                readAllLines(Paths.get("src/main/resources/ssn/subClass.txt"),
+        List<String> ssnClassList = Files.
+                readAllLines(Paths.get("src/main/resources/ssn/classList.txt"),
                         Charset.defaultCharset());
-        String classQuery  = IOUtils.readFile("ssn/class.rq", Charset.defaultCharset());
 
-
-        List<String> propList = Files.
+        List<String> ssnPropertyList = Files.
                 readAllLines(Paths.get("src/main/resources/ssn/propertyList.txt"),
                         Charset.defaultCharset());
-        String propertyQuery  = IOUtils.readFile("ssn/prop.rq", Charset.defaultCharset());
+
+
+        List<String> subClassList = Files.
+                readAllLines(Paths.get("src/main/resources/ssn/subClass.txt"),
+                        Charset.defaultCharset());
 
         List<String> v201604List = Files.
                 readAllLines(Paths.get("src/main/resources/owled/p.txt"),
                         Charset.defaultCharset());
 
-        for (String clazz : classList) {
 
-            ParameterizedSparqlString pss = new ParameterizedSparqlString();
-            pss.setCommandText(classQuery);
-            pss.setIri("class", clazz);
-
-            String queryString = pss.toString();
-            System.out.println(queryString);
-
-            long count = SparqlUtils.executeQueryForLong(queryString, lodCache, "c");
-
-            if (count > 0) {
-                System.out.println(clazz + "," + count);
-            }
-        }
-
-        System.out.println("Done!");
+        //checkInstances(ssnClassList, aemetSparql);
+        check4triples(ssnPropertyList, aemetSparql);
 
 
 //        List<String> classList = Files.
@@ -88,5 +95,51 @@ public class DatasetCheck {
 //        }
 
     }
+
+
+    public static void checkInstances(List<String> classList, String endpoint) {
+
+        for (String clazz : classList) {
+
+            ParameterizedSparqlString pss = new ParameterizedSparqlString();
+            pss.setCommandText(classQuery);
+            pss.setIri("class", clazz);
+
+            String queryString = pss.toString();
+            System.out.println(queryString);
+
+            long count = SparqlUtils.executeQueryForLong(queryString, endpoint, "c");
+
+            if (count > 0) {
+                System.out.println(clazz + "," + count);
+            }
+        }
+
+        System.out.println("Done!");
+
+    }
+
+    public static void check4triples(List<String> propertyList, String endpoint) {
+
+        for (String property : propertyList) {
+
+            ParameterizedSparqlString pss = new ParameterizedSparqlString();
+            pss.setCommandText(propertyQuery);
+            pss.setIri("p", property);
+
+            String queryString = pss.toString();
+
+            long count = SparqlUtils.executeQueryForLong(queryString, endpoint, "c");
+
+            if (count > 0) {
+                System.out.println(property + "," + count);
+            }
+        }
+
+        System.out.println("Done!");
+
+    }
+
+
 
 }
